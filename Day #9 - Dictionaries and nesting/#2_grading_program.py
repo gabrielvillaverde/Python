65bf773f4635970daa5addad107a3b472ddb1557
gbr.villaverde <gbr.villaverde@gmail.com>
2023-10-06 00:01:13 -0300

    Day 9 Progress

---
+++ Day #9 - Dictionaries and nesting/#2_grading_program.py
@@ -0,0 +1,26 @@
+# Program that puts into practice the use of dictionaries. 
+# In the first dictionary, there are students along with their grades. 
+# In the second dictionary, the goal is to display the student's name along with the category they belong to based on their grade.
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+student_grades = {}
+
+for student in student_scores:
+  score = student_scores[student]
+  if score > 90:
+    student_grades[student] = "Outstanding"
+  elif score > 80:
+    student_grades[student] = "Exceeds Expectations"
+  elif score > 70:
+    student_grades[student] = "Acceptable"
+  else:
+    student_grades[student] = "Fail"
+
+print(student_grades)
